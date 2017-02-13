d9b5c53daded36c350bca3bc08bc5935eb69aa23
Leonardo Silva <leonardo@lead-web.com>
2017-02-12 22:53:54 -0500

    First commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.jcesarmobile.filepicker.FilePicker",
+        "file": "plugins/com.jcesarmobile.filepicker/www/FilePicker.js",
+        "pluginId": "com.jcesarmobile.filepicker",
+        "clobbers": [
+            "FilePicker"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.jcesarmobile.filepicker": "1.1.1",
+    "cordova-plugin-whitelist": "1.3.1"
+};
+// BOTTOM OF METADATA
+});
